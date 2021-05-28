66442a21d7c5f607047486f33ac3a24a3c52e940
Pedro Ananias <pedro.ananias@unesp.br>
2021-05-28 16:23:01 -0300

    - Minor fixes in batch

--- batch.30.sh
+++ batch.30.sh
@@ -9,9 +9,10 @@ SCRIPT="script.py"
 CLEAR="sudo pkill -f /home/pedro/anaconda3"
 
 # ATTRIBUTES
-declare -a GRID_SIZES=(7)
-declare -a MODELS=("lstm") # "rf" "svm" "mlp" "lstm"
-declare EXTRA="--days_threshold=180 --days_in=4 --days_out=5"
+declare -a DAYS_THRESHOLD=("60" "90" "180" "365" "730") # "90" "180" "365" "730"
+declare -a REDUCTIONS=("median" "min") # "median" "min"
+declare -a MODELS=("rf" "svm" "lstm") # "rf" "svm" "mlp" "lstm"
+declare EXTRA="--grid_size=7 --days_in=4 --days_out=5"
 
 # SHOW BASE DIR
 echo "$PYTHON $BASEDIR/$SCRIPT"
@@ -23,13 +24,16 @@ LAT_LON="85.08856074928927,19.698732779758075,85.28279700936078,19.5468193549138
 declare -a DATES=("2014-11-25")
 
 # EXECUTIONS
-for model in "${MODELS[@]}"
+for reduction in "${REDUCTIONS[@]}"
 do
-	for date in "${DATES[@]}"
+	for model in "${MODELS[@]}"
 	do
-		for grid_size in "${GRID_SIZES[@]}"
+		for date in "${DATES[@]}"
 		do
-			eval "$PYTHON $BASEDIR/$SCRIPT --lat_lon=$LAT_LON --name=$NAME --from_date=$date --model=$model --grid_size=$grid_size $EXTRA"
+			for day_threshold in "${DAYS_THRESHOLD[@]}"
+			do
+				eval "$PYTHON $BASEDIR/$SCRIPT --lat_lon=$LAT_LON --name=$NAME --from_date=$date --model=$model --days_threshold=$day_threshold --reduction=$reduction $EXTRA"
+			done
 		done
 	done
 done
@@ -45,13 +49,16 @@ LAT_LON="-83.48811946836814,41.85776095627803,-83.18290554014548,41.677617395337
 declare -a DATES=("2013-10-11" "2015-07-27")
 
 # EXECUTIONS
-for model in "${MODELS[@]}"
+for reduction in "${REDUCTIONS[@]}"
 do
-	for date in "${DATES[@]}"
+	for model in "${MODELS[@]}"
 	do
-		for grid_size in "${GRID_SIZES[@]}"
+		for date in "${DATES[@]}"
 		do
-			eval "$PYTHON $BASEDIR/$SCRIPT --lat_lon=$LAT_LON --name=$NAME --from_date=$date --model=$model --grid_size=$grid_size $EXTRA"
+			for day_threshold in "${DAYS_THRESHOLD[@]}"
+			do
+				eval "$PYTHON $BASEDIR/$SCRIPT --lat_lon=$LAT_LON --name=$NAME --from_date=$date --model=$model --days_threshold=$day_threshold --reduction=$reduction $EXTRA"
+			done
 		done
 	done
 done
@@ -67,13 +74,16 @@ LAT_LON="119.85091169141491,31.553377091606887,120.64485195719814,30.91800041328
 declare -a DATES=("2016-08-26" "2017-07-25")
 
 # EXECUTIONS
-for model in "${MODELS[@]}"
+for reduction in "${REDUCTIONS[@]}"
 do
-	for date in "${DATES[@]}"
+	for model in "${MODELS[@]}"
 	do
-		for grid_size in "${GRID_SIZES[@]}"
+		for date in "${DATES[@]}"
 		do
-			eval "$PYTHON $BASEDIR/$SCRIPT --lat_lon=$LAT_LON --name=$NAME --from_date=$date --model=$model --grid_size=$grid_size $EXTRA"
+			for day_threshold in "${DAYS_THRESHOLD[@]}"
+			do
+				eval "$PYTHON $BASEDIR/$SCRIPT --lat_lon=$LAT_LON --name=$NAME --from_date=$date --model=$model --days_threshold=$day_threshold --reduction=$reduction $EXTRA"
+			done
 		done
 	done
 done
